deebce831554fe634106ed5126b1f3b9c9575b60
Brindha S <brinsree@gmail.com>
2018-04-13 01:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine,newName){
 }
 function nowServing(){
   if(katzDeliLine.length===0){
-    return "There is nobody watiting to be served";
+    return "There is nobody waiting to be served!";
   }else{
     return katzDeliLine.shift();
   }
