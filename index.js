a4cf48269e2e134a37a310bf842c3b78b1df3268
Brindha S <brinsree@gmail.com>
2018-04-13 01:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var katzDeliLine=[];
 function takeANumber(katzDeliLine,newName){
   katzDeliLine.push(newName);
-var pos=katzDeliLine.length+1;
-return `${newName},you are ${pos}th in line`;
+ var pos=katzDeliLine.length+1;
+ return `${newName},you are ${pos}th in line`;
   
 }
 function nowServing(){
-  
+  if(katzDeliLine.length===0){
+    return "There is nobody watiting to be served";
+  }else{
+    
+  }
 }
