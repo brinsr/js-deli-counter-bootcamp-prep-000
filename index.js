792270619eec4f6c026d590a8d44f38b3da28f77
Brindha S <brinsree@gmail.com>
2018-04-13 02:08:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ function takeANumber(katzDeliLine,newName){
   
 }
 function nowServing(){
-  if(katzDeliLine.length===0){
-    return "There is nobody waiting to be served!";
-  }else{
+  //if(katzDeliLine.length===0){
+    //return "There is nobody waiting to be served!";
+  //}else{
     var personNowServing=katzDeliLine.shift();
     return `Currently serving ${personNowServing}`;
-  }
+  //}
 }
 function currentLine(katzDeliLine){
   if(katzDeliLine.length===0){
     
   return "The line is currently empty.";
-} else {
+  } else {
      var addTo="";
      for(var i=0;i<katzDeliLine.length-1;i++){
           addTo+= `${i+1}. ${katzDeliLine[i]}, `;
         }
         addTo+=`${katzDeliLine.length}. ${katzDeliLine[katzDeliLine.length-1]}`;
-  return "The line is currently: "+addTo;
+       return "The line is currently: "+addTo;
 }
 }
