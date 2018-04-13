f9a0c437231257d7ef3c3a8dd0421c495a3f2793
Brindha S <brinsree@gmail.com>
2018-04-13 01:54:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function currentLine(katzDeliLine){
 } else {
      var addTo="";
      for(var i=0;i<katzDeliLine.length-1;i++){
-          addTo+= `${i+1}. ${katzDeliLine[i]}, `;
+          addTo+= `${i+1}.${katzDeliLine[i]}, `;
         }
-        addTo+=`${katzDeliLine.length}.`;
+        addTo+=`${katzDeliLine.length}.${katzDeliLine[length-1]}`;
   return "The line is currently: "+addTo;
 }
 }
